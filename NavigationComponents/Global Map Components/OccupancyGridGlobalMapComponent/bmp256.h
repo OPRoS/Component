2e7172fcd51e79af4640fadeb33fa9df8c342791
yudonguk <yudonguk@naver.com>
2013-11-08 10:48:30 +0900

    remove duplicated file

--- NavigationComponents/Global Map Components/OccupancyGridGlobalMapComponent/bmp256.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-extern unsigned char *ReadBmp256 (const char *fileName, int &width, int &height, unsigned char palette[256][4]);
-extern int WriteBmp256 (const char *fileName, int width, int height, unsigned char *image, unsigned char palette[256][4]);
-extern void FreeImage (unsigned char *image);
